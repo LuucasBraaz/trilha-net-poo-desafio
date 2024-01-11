32a39485f45a84d690b272fd5c38286e24362bc5
LuucasBraaz <lucasantos.91@outlook.com>
2024-01-10 21:53:12 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,66 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Solicitar ao usuário os dados para Nokia
+            Console.WriteLine("Digite os dados para o Nokia:");
+            Nokia nokiaPhone = DadosDeEntradaNokia();
+
+            // Teste de instalação de aplicativo no Nokia
+            TestarSmartphone(nokiaPhone, "Ifood");
+
+            // Solicitar ao usuário os dados para iPhone
+            Console.WriteLine("Digite os dados para o iPhone:");
+            Iphone iphone = DadosDeEntradaIphone();
+
+            // Teste de instalação de aplicativo no iPhone
+            TestarSmartphone(iphone, "Facebook");
+        }
+
+        static Nokia DadosDeEntradaNokia()
+        {
+            Console.Write("Número: ");
+            string numero = Console.ReadLine();
+
+            Console.Write("Modelo: ");
+            string modelo = Console.ReadLine();
+
+            Console.Write("IMEI: ");
+            string imei = Console.ReadLine();
+
+            Console.Write("Memória: ");
+            int memoria = int.Parse(Console.ReadLine());
+
+            return new Nokia(numero, modelo, imei, memoria);
+        }
+
+        static Iphone DadosDeEntradaIphone()
+        {
+            Console.Write("Número: ");
+            string numero = Console.ReadLine();
+
+            Console.Write("Modelo: ");
+            string modelo = Console.ReadLine();
+
+            Console.Write("IMEI: ");
+            string imei = Console.ReadLine();
+
+            Console.Write("Memória: ");
+            int memoria = int.Parse(Console.ReadLine());
+
+            return new Iphone(numero, modelo, imei, memoria);
+        }
+
+        static void TestarSmartphone(Smartphone smartphone, string aplicativo)
+        {
+            smartphone.Ligar();
+            smartphone.ReceberLigacao();
+            smartphone.InstalarAplicativo(aplicativo);
+            Console.WriteLine();
+        }
+    }
+}
